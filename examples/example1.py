b1201edc4bc7b8d893c2f0793d34f309efec116a
Marko Tasic <mtasic85@gmail.com>
2011-03-25 21:23:05 +0100

    fixed: cleaned up example1 is basic hello world example

--- examples/example1.py
+++ examples/example1.py
@@ -2,26 +2,33 @@ import sys
 sys.path.append('..')
 
 import gir
-import gir._girepository
-
 rep = gir.GIRepository()
-GLib = rep.GLib
-# GLib._wrap_all()
-
-GObject = rep.GObject
-# GObject._wrap_all()
-
 Gtk = rep.Gtk
-# Gtk._wrap_all()
+
+def cb_window_delete_event(window, *args, **kwargs):
+	print('cb_window_delete_event:', window, args, kwargs)
+	return False
 
 def cb_window_destroy(window, *args, **kwargs):
 	print('cb_window_destroy:', window, args, kwargs)
+	Gtk.main_quit()
+
+def cb_button_clicked(button, *args, **kwargs):
+	print('cb_button_clicked:', window, args, kwargs)
 	Gtk.main_quit()
 
 if __name__ == '__main__':
 	Gtk.init(0, [])
 	window = Gtk.Window.new(Gtk.WindowType.toplevel)
 	window.set_title('Test 1')
+	vbox = Gtk.VBox.new(False, 0)
+	label = Gtk.Label.new('Hello world!')
+	vbox.pack_start(label, True, True, 10)
+	button = Gtk.Button.new_from_stock('gtk-quit')
+	button.connect('clicked', cb_button_clicked)
+	vbox.pack_start(button, False, True, 0)
+	window.add(vbox)
 	window.show_all()
-	window.connect('destroy', cb_window_destroy, None, a=10)
+	window.connect('delete-event', cb_window_delete_event)
+	window.connect('destroy', cb_window_destroy)
 	Gtk.main()

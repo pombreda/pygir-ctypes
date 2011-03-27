fe047417c45e4c35ce92a00ffef1b8a62b2948e3
Marko Tasic <mtasic85@gmail.com>
2011-03-27 22:06:05 +0200

    updated: test8

--- tests/test8.py
+++ tests/test8.py
@@ -2,22 +2,25 @@
 class A(object):
 	@classmethod
 	def new(cls, *args, **kwargs):
+		print('A.new', cls, args, kwargs)
 		return cls(_self=None, *args, **kwargs)
 	
 	def __new__(cls, *args, **kwargs):
+		print('A.__new__', cls, args, kwargs)
 		self = super(A, cls).__new__(cls, *args, **kwargs)
 		return self
 	
 	def __init__(self, *args, **kwargs):
-		pass
+		print('A.__init__', self, args, kwargs)
 
 class B(A):
 	def __new__(cls, *args, **kwargs):
+		print('B.__new__', cls, args, kwargs)
 		self = super(B, cls).__new__(cls, *args, **kwargs)
-		B.__init__(self, *args, **kwargs)
 		return self
 	
 	def __init__(self, *args, **kwargs):
+		print('B.__init__', self, args, kwargs)
 		A.__init__(self, *args, **kwargs)
 
 b = B()

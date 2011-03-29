1ddd507829dba3402aefacc47caafb04ebf64778
Marko Tasic <mtasic85@gmail.com>
2011-03-30 00:19:48 +0200

    fixed: setup

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from distutils.core import setup
 setup(
 	name='pygir-ctypes',
 	version='0.1.0',
-	description='Pure Python GObject Introspection Repository (GIR) wrapper using ctypes',
+	description='Python GObject Introspection Repository',
+	long_description='Pure Python GObject Introspection Repository (GIR) wrapper using ctypes',
 	author='Marko Tasic',
 	author_email='mtasic85@gmail.com',
 	url='http://code.google.com/p/pygir-ctypes/',
 	packages=['gir'],
 	license='New BSD License',
 	platforms=['Any'],
+	data_files=[('', ['LICENSE'])],
 )
